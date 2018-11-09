0d1ce4150920e3495ffe44757a1a0aa7341075a9
CodyGrinstead <yodacodyone@gmail.com>
2018-11-09 10:50:14 -0700

    working lock, needs calibration

--- Padlock/padlock_test/padlock_test.ino
+++ Padlock/padlock_test/padlock_test.ino
@@ -3,122 +3,95 @@
 #define GREEN 9
 #define BLUE 7
 #define RED 4
+#define LED 3
 int left_light;
 int right_light;
-bool inOrder=false;
-bool left1=false;
-bool right1=false;
-bool left2=false;
-bool right2=false;
-int combo=0;
+int center_light;
+char code[4];
+
 
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE,OUTPUT);
   pinMode(RED, OUTPUT);
+  pinMode(LED,OUTPUT);
+  left_light = analogRead(A0);
+  right_light = analogRead(A1);
+  center_light=analogRead(A2);
   Serial.begin(9600);
 
 }
 
+bool is_order(char code[4])
+{
+  //this is the code you want
+  char ans[4]="rrrr";
+  bool ret=false;
+  for(int i=0;i<4;i++)
+  {
+    if(code[i]!=ans[i])
+    {
+      ret=false;
+      Serial.print(i);
+      Serial.print(code[i]);
+      Serial.println(ans[i]);
+      break;
+    }
+    else ret=true;    
+  }
+  
+  return ret;
+}
+
+char censor_check()
+{
+  char ans;
+  if(left_light <150){
+    ans='l';
+    digitalWrite(GREEN,HIGH);
+    delay(500);
+    digitalWrite(GREEN,LOW);
+  }
+  else if(right_light<150){
+    ans='r';
+    digitalWrite(BLUE,HIGH);
+    delay(250);
+    
+    digitalWrite(BLUE,LOW);
+  }
+  else if(center_light<150){
+    ans='c';
+    digitalWrite(RED,HIGH);
+    delay(500);
+    
+  }
+  else ans='f';
+  delay(1500);
+  return ans;
+}
+
+
 void loop() {
 
-  left_light = analogRead(A0);
-  right_light = analogRead(A1);
-
-  //check 1st input
-  if(combo==0 && left_light > 150){
-    left1=true;
-    combo=1;
-    digitalWrite(GREEN,HIGH);
-  }
-  //check 2nd input
-  if(combo==1){
-    //check for false before true
-    if(left_light >150){
-      left1=false;
-      combo=0;
-    }
-    if(right_light >150 &&combo==1){
-      right1=true;
-      combo=2;
-      digitalWrite(BLUE,HIGH);
-    }
-  }
-//check 3rd input
-  if(combo==2){
-    if(right_light >150){
-      left1=false;
-      right1=false;
-      combo=0;
-    }
-    if(left_light >150&&combo==2){
-      left2=true;
-      combo=3;
-      digitalWrite(GREEN,HIGH);
-    }    
-  }
-  //check 4th input
-  if(combo==3){
-    if(left_light>150){
-      left1=false;
-      left2=false;
-      right1=false;
-      combo=0;
-    }
-    if(right_light>150&&combo==3){
-      right2=true;
-      combo=4;
-      digitalWrite(BLUE,HIGH);
-    }
-  }
-  //check if combo is finshed
-  if(combo=4){
-    if(left1==true && left2==true &&right1==true &&right2==true){
-      inOrder=true;
-    }
-  }
-  if(inOrder==true) digitalWrite(RED, HIGH);
-  //delay xsec where 1sec=1000 between each check
-  delay(3000);
-
-  
+  Serial.println(center_light);
+  Serial.println(right_light);
+  Serial.println(left_light);
+  for(int i=0;i<4;++i){
+    code[i]=censor_check();
     
-  
- 
-
-  
- /*
-
-  if(left_light < 150){
-    if(left1==false){
-      left1=true;    
+    digitalWrite(GREEN,LOW);
+    digitalWrite(RED,LOW);
+    while(code[i]=='f')
+    {
+      code[i]=censor_check();    
     }
-    if(left1==true && right1==true) {
-      left2=true;
-      
-    }
-    digitalWrite(GREEN,HIGH);
-  }
-  else digitalWrite(GREEN,LOW);
-
-  if(right_light < 150){
-      if(right1==false)right1=true;
-      if(right1==true && left2==true) {
-        right2=true;
-        Serial.println(right2);
-      }
     
     
-    digitalWrite(BLUE,HIGH);
   }
-  else digitalWrite(BLUE,LOW);
-
-  
-  if(left1=true && right1==true && left2==true && right2==true) inOrder=true;
-  if(inOrder==true) digitalWrite(RED, HIGH);
-  */
-  //delay(2000);
+  if(is_order(code)==true){
+    digitalWrite(LED,HIGH);
+    Serial.write("Code is true");
+  }
 }
   
